d841d6aebbf0071c96e076953a55c8597021e3ac
eduardocampigoto <eduardo.campigoto@gmail.com>
2020-07-14 02:41:15 -0300

    update keys

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use('/popper', express.static(__dirname + '/node_modules/popper.js/dist'));
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
-const BASE_API = "http://localhost:8080/api";
+const BASE_API = " https://aceville.herokuapp.com/api";
 const getSettings = { method: "GET" };
 const postSettings = { method: 'POST' };
 
